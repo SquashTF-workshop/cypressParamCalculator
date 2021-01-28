d0767a10637e291aed1011767bba385308772e1b
UKevin94 <kevinung94@gmail.com>
2021-01-28 16:16:51 +0100

    Fixed CUFs name

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -2,21 +2,21 @@ describe('Calculator', () => {
   it('add', () => {
     var a = 5
     var b = 12
-    expect(a + b).to.equal(parseInt(Cypress.env('CUF_CPG_add_result')))
+    expect(a + b).to.equal(parseInt(Cypress.env('CPG_CUF_add_result')))
   })
   it('mult', () => {
     var a = 2
     var b = 4
-    expect(a * b).to.equal(parseInt(Cypress.env('CUF_IT_mult_result')))
+    expect(a * b).to.equal(parseInt(Cypress.env('IT_CUF_mult_result')))
   })
   it('sub', () => {
     var a = 10
     var b = 5
-    expect(a - b).to.equal(parseInt(Cypress.env('CUF_TC_sub_result')))
+    expect(a - b).to.equal(parseInt(Cypress.env('TC_CUF_sub_result')))
   })
   it('div', () => {
     var a = 10
     var b = 5
-    expect(a / b).to.equal(parseInt(Cypress.env('CUF_TS_div_result')))
+    expect(a / b).to.equal(parseInt(Cypress.env('TS_CUF_div_result')))
   })
 })
